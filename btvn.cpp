f84f71aba4941e1d1c7cb2447303a941de5867fd
ducbruhlmao <ducnx.24810310167@epu.edu.vn>
2025-09-13 01:07:07 +0700

    upload code

--- btvn.cpp
+++ btvn.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int *NhapMang(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("a[%d]=", i + 1);
+        scanf("%d", &a[i]);
+    }
+    return a;
+}
+
+void InMang(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("a[%d]=%d\n", i + 1, a[i]);
+    }
+}
 
 int main()
 {
+    int *arr;
+    int n;
+    printf("Nhap so luong phan tu: ");
+    scanf("%d", &n);
+    arr = (int *)malloc(n * sizeof(int));
+    NhapMang(arr, n);
+    InMang(arr, n);
+    free(arr);
     return 0;
 }
