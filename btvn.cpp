5538c0bccfe63d8f0de840e0e6778ea5d6be8913
ducbruhlmao <ducnx.24810310167@epu.edu.vn>
2025-09-13 01:41:26 +0700

    upload code

--- btvn.cpp
+++ btvn.cpp
@@ -18,6 +18,36 @@ void InMang(int *a, int n)
         printf("a[%d]=%d\n", i + 1, a[i]);
     }
 }
+int everage(int *a, int n)
+{
+    float total = 0.0;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] < 0)
+        {
+            total += a[i];
+            count++;
+        }
+    }
+    if (count == 0)
+        return 0;
+    else
+        return total / count;
+}
+
+void increase(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
 
 int main()
 {
@@ -28,6 +58,10 @@ int main()
     arr = (int *)malloc(n * sizeof(int));
     NhapMang(arr, n);
     printf("Mảng đã nhập: \n");
+    InMang(arr, n);
+    printf("TBC: %.2f", everage(arr, n));
+    printf("\nMảng được sắp xếp tăng dần: \n");
+    increase(arr, n);
     InMang(arr, n);
     free(arr);
     return 0;
