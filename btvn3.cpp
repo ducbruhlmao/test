daad5cbe782ed80b7c34ae3c1db9c0c1c2d93a64
ducbruhlmao <ducnx.24810310167@epu.edu.vn>
2025-09-15 09:53:36 +0700

    upload

---
+++ btvn3.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Nhap_Mang(float *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("a[%d]=", i);
+        scanf("%f", &a[i]);
+    }
+}
+
+void In_Mang(float *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%.2f\n", a[i]);
+    }
+}
+float Minn(float *a, int n)
+{
+    int min = 9999;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+    }
+    return min;
+}
+int main()
+{
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    float *a;
+    a = (float *)malloc(n * sizeof(float));
+    Nhap_Mang(a, n);
+    printf("Mang vua nhap: \n");
+    In_Mang(a, n);
+    printf("Phan tu nho nhat trong mang: %.2f", Minn(a, n));
+    return 0;
+}
