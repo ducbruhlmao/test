46001955d72668d8dbcbb5b0cec7e4fb016012a0
ducbruhlmao <ducnx.24810310167@epu.edu.vn>
2025-09-18 09:18:16 +0700

    upload

---
+++ btvn5.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Nhap_Mang(int **a, int n)
+{
+    *a = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("a[%d]=", i);
+        scanf("%d", &(*a)[i]);
+    }
+}
+void In_Mang(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+}
+void chia_ba(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % 3 == 0 && a[i] < 50)
+        {
+            printf("%d\t", a[i]);
+        }
+    }
+}
+void add_x(int **a, int n, int x, int k)
+{
+    for (int i = k; i <= n; i++)
+    {
+        a[i + 1] = a[i];
+    }
+    *a[k] = x;
+    if (k > n)
+    {
+        *a[n] = x;
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d\t", *a[i]);
+    }
+}
+int main()
+{
+    int *a;
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    Nhap_Mang(&a, n);
+    printf("Mang vua nhap: \n");
+    In_Mang(a, n);
+    printf("\nCác phần tử chia hết cho 3 và nhỏ hơn 50:\n");
+    chia_ba(a, n);
+    int x;
+    printf("\nNhap x: ");
+    scanf("%d", &x);
+    int k;
+    printf("\nNhap vi tri: ");
+    scanf("%d", &k);
+    printf("\nMang sau khi chen:\n");
+    add_x(&a, n, x, k);
+    In_Mang(a, n);
+    free(a);
+    return 0;
+}
